ceaa8173e187af69df2c6da11f1da24ad9704ba1
PrimoBrowser Management <Alex@primobrowser.com>
2025-09-14 17:45:38 -0500

    re-add stdafx.h after lowercase rename

---
+++ stdafx.h
@@ -0,0 +1,31 @@
+// stdafx.h : 標準のシステム インクルード ファイル、
+//            または参照回数が多く、かつあまり変更されない
+//            プロジェクト専用のインクルード ファイルを記述します。
+//
+
+#if !defined(AFX_STDAFX_H__95DDB94E_4F4D_4D1C_B05F_FB0C8B9E6423__INCLUDED_)
+#define AFX_STDAFX_H__95DDB94E_4F4D_4D1C_B05F_FB0C8B9E6423__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define VC_EXTRALEAN		// Windows ヘッダーから殆ど使用されないスタッフを除外します。
+
+#ifndef WINVER				// Allow use of features specific to Windows XP or later.
+#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#include <afxwin.h>         // MFC のコアおよび標準コンポーネント
+#include <afxext.h>         // MFC の拡張部分
+#include <afxdtctl.h>		// MFC の Internet Explorer 4 コモン コントロール サポート
+#include <afxstr.h>
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC の Windows コモン コントロール サポート
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ は前行の直前に追加の宣言を挿入します。
+
+#endif // !defined(AFX_STDAFX_H__95DDB94E_4F4D_4D1C_B05F_FB0C8B9E6423__INCLUDED_)
